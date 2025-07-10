95b6e9c5a40276c8787e8b09e34763a100124a39
Davide <keeshux@gmail.com>
2025-07-10 10:22:58 +0200

    Generalize package

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SubjectStreams",
+    name: "cross-platform-swift",
     platforms: [
         .iOS(.v13),
         .macOS(.v10_15),
@@ -13,15 +13,12 @@ let package = Package(
         .visionOS(.v1)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "SubjectStreams",
+            name: "cross-platform-swift",
             targets: ["SubjectStreams"]
         )
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SubjectStreams"
         ),
--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-Simple replacements for Combine subjects with AsyncStream and Swift 6 Concurrency.
+# Cross-platform Swift
 
-This is an excerpt from my (ongoing) series about cross-platform Swift:
+These are excerpts from my (ongoing) series about cross-platform Swift:
 
 https://davidederosa.com/cross-platform-swift/combine/
 
 The series is about how I'm porting [Passepartout](https://github.com/passepartoutvpn/passepartout), my Swift/SwiftUI VPN client, beyond the Apple ecosystem.
+
+## SubjectStreams
+
+Simple replacements for Combine subjects with AsyncStream and Swift 6 Concurrency.

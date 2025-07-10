5d0aa297ce16aa88278333b6e9d05fcaca662dd0
Davide <keeshux@gmail.com>
2025-07-10 10:57:22 +0200

    Complete add/accumulate

--- Tests/CInteropTests/CInteropTests.swift
+++ Tests/CInteropTests/CInteropTests.swift
@@ -35,6 +35,7 @@ struct CInteropTests {
     @Test
     func add() {
         #expect(cps_add(10, 20) == 30)
+        #expect(cps_inline_add(10, 20) == 30)
     }
 
     @Test
@@ -48,10 +49,26 @@ struct CInteropTests {
 
     @Test
     func accumulate() {
+        // the array type DOES make a difference, because Int is typically 64-bit
+//        let v: [Int] = [10, 20, 30, 40]
         let v: [Int32] = [10, 20, 30, 40]
         v.withUnsafeBytes {
+            // the bound type must match declaration
             let pv = $0.bindMemory(to: Int32.self)
             #expect(cps_accumulate(pv.baseAddress, v.count) == 100)
         }
+    }
+
+    @Test
+    func accumulateAndStore() {
+        let v: [Int32] = [10, 20, 30, 40]
+        // trying to use Int here will give you legendary headaches
+//        var result: Int = 0
+        var result: Int32 = 0
+        v.withUnsafeBytes {
+            let pv = $0.bindMemory(to: Int32.self)
+            cps_accumulate_and_store(pv.baseAddress, v.count, &result)
+        }
+        #expect(result == 100)
     }
 }
